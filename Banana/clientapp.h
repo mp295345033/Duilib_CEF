d71e8f14faa45df13bd8e78770961e89547d7aee
zhenghn <2839496190@qq.com>
2019-09-02 11:23:37 +0800

    111

---
+++ Banana/clientapp.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "stdAfx.h"
+
+class CCefClientApp : public CefApp, public CefBrowserProcessHandler, CefRenderProcessHandler
+{
+public:
+	CCefClientApp();
+	~CCefClientApp();
+
+
+	virtual CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() {
+		return this;
+	}
+
+	// CefApp methods:
+	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override;
+	virtual void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line) override;
+
+
+	// CefBrowserProcessHandler methods:
+	virtual void OnContextInitialized() override;
+	//CefRenderProcessHandler methods
+
+//	virtual CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler()  override;
+
+	virtual void OnWebKitInitialized() override;
+	virtual void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context)  override;
+	virtual void OnContextReleased(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) override;
+
+
+private:
+	//CefRefPtr<CCEFV8HandlerEx> v8Handler_;
+	// Include the default reference counting implementation.
+	IMPLEMENT_REFCOUNTING(CCefClientApp);
+
+private:
+
+};
